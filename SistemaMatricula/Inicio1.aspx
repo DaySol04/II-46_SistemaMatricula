c55ffee34064dc9ca9197b23a964b4346d7625b5
DaySol04 <125769097+DaySol04@users.noreply.github.com>
2026-02-24 17:16:35 -0600

    css

---
+++ SistemaMatricula/Inicio1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Inicio1.aspx.vb" Inherits="SistemaMatricula.Inicio1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<link href="Inicio.css" rel="stylesheet" />
+
+    <div class="inicio-container">
+        <h1>Universidad</h1>
+        <p>Sistema de Matrícula</p>
+
+        <!-- Botón para Registro de Estudiantes -->
+        <asp:HyperLink ID="lnkEstudiantes" runat="server" NavigateUrl="~/Registro Estudiantes.aspx" Text="Registro de Estudiantes" CssClass="btn" />
+
+        <!-- Botón para Carreras -->
+        <asp:HyperLink ID="lnkCarreras" runat="server" NavigateUrl="~/Carreras.aspx" Text="Listado de Carreras" CssClass="btn btn-green" />
+    </div>
+
+</asp:Content>
---
+++ SistemaMatricula/Inicio1.aspx.designer.vb
@@ -0,0 +1,33 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Inicio1
+
+    '''<summary>
+    '''Control lnkEstudiantes.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lnkEstudiantes As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control lnkCarreras.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lnkCarreras As Global.System.Web.UI.WebControls.HyperLink
+End Class
